dda06dee00d12541aea990543cd80cc2e9cf1e39
NumiBlue <thekindlyblog@gmail.com>
2022-06-08 23:24:20 -0400

    caching set up

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,15 @@
+const appName = "Budget-Tracker";
+ const version = "v1";
+ const CACHE_NAME = appName + version;
+ const CACHE_FILES = [
+   "./index.html",
+   "./css/style.css",
+   "./js/idb.js",
+   "./js/index.js",
+ ];
+
+
+
 self.addEventListener("install", function (event) {
     console.log("Service worker installed!");
   });
