17a91208ecc3689c028b74e3006d0289f3cf6343
NumiBlue <thekindlyblog@gmail.com>
2022-06-08 23:18:27 -0400

    activates service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,8 @@
 self.addEventListener("install", function (event) {
     console.log("Service worker installed!");
   });
+
+  //Activate
+  self.addEventListener("activate", function (event) {
+    console.log("Service worker activated!");
+  });
