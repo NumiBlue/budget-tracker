cba9fb83deb724d49d264be88c5ba774217af7b6
NumiBlue <thekindlyblog@gmail.com>
2022-06-08 23:47:40 -0400

    adds beginning od index.js

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+let transactions = [];
+ let myChart;
+
+ fetch("/api/transaction")
+   .then((response) => {
+     return response.json();
+   })
+   .then((data) => {
+     // save db data as global variable
+     transactions = data;
+
+     populateTotal();
+     populateTable();
+     populateChart();
+   });
+ function populateTotal() {
+   // reduce transaction amounts to a single total value
+   let total = transactions.reduce((total, t) => {
+     return total + parseInt(t.value);
+   }, 0);
+   let totalEl = document.querySelector("#total");
+   totalEl.textContent = total;
+ }
+ function populateTable() {
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
